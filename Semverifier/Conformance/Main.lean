78095da5aa3c83f485df9d063712b4813190da8d
shumoku88-bit <shumoku88@gmail.com>
2026-09-18 18:30:56 +0900

    test: emit Lean conformance judgments

---
+++ Semverifier/Conformance/Main.lean
@@ -0,0 +1,19 @@
+import Semverifier.Conformance.Corpus
+
+open Semverifier
+
+namespace Semverifier.Conformance
+
+private def emitCase (rangeRaw versionRaw : String) : IO Unit := do
+  match Range.parse? rangeRaw, Version.parse? versionRaw with
+  | some range, some version =>
+      IO.println s!"{rangeRaw}\t{versionRaw}\t{range.satisfies version}"
+  | _, _ =>
+      throw <| IO.userError s!"invalid generated conformance case: {rangeRaw} / {versionRaw}"
+
+def main : IO Unit := do
+  for rangeRaw in ranges do
+    for versionRaw in versions do
+      emitCase rangeRaw versionRaw
+
+end Semverifier.Conformance
